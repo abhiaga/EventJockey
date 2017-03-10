41da68cbe4ff78904ff5bccf922e3ab18b8b7666
abhacharya <abhi21.aga@gmail.com>
2017-03-10 23:52:35 +0530

    this is most new

--- app/src/main/java/com/example/abhacharya/eventjockey/FirstEventActivity.java
+++ app/src/main/java/com/example/abhacharya/eventjockey/FirstEventActivity.java
@@ -2,8 +2,6 @@ package com.example.abhacharya.eventjockey;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,13 +14,14 @@ import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 public class FirstEventActivity extends AppCompatActivity {
-
+    private static final String LOG_TAG = "PlaceSelectionListener";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_event);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         Button button = (Button) findViewById(R.id.button2);
+
         setSupportActionBar(toolbar);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -34,18 +33,25 @@ public class FirstEventActivity extends AppCompatActivity {
 
         PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        /*autocompleteFragment.setBoundsBias(new LatLngBounds(
+                new LatLng( 12.97232, 77.59480 ),
+                new LatLng(12.89201, 77.58905)));*/
+
 
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
                 // TODO: Get info about the selected place.
+                System.out.println(place.getName());
                 //Log.i(TAG, "Place: " + place.getName());
             }
 
             @Override
             public void onError(Status status) {
                 // TODO: Handle the error.
-               // Log.i(TAG, "An error occurred: " + status);
+                System.out.println("hi1");
+                 Log.e(LOG_TAG, "onError: Status = " + status.toString());
+
             }
         });
 
